ad7eacca68e3ba7697d2df053e499102fc49565f
Phuong Cao <caophuong2012@gmail.com>
2017-09-11 16:20:43 +0700

    Update install_docker_ruby_rails.sh
    - Exec in correct web directory
    - Install proccps and net-tools

--- install_docker_ruby_rails.sh
+++ install_docker_ruby_rails.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-echo "This script is tutored to install Ruby on docker"
+echo "This script is to install Ruby on docker"
 [ ! -z "$1" ] && docker_name=$1 || docker_name="ruby" 
-[ ! -z "$2" ] && docker_port=$2 || docker_port="3000"
+[ ! -z "$2" ] && docker_port=$2 || docker_port="80"
 docker_web_port="3000"
+rails_name="my-blog"
+web_dir="/root/$rails_name"
 
 #docker create --name $docker_name -ti debian:latest
 docker run --name $docker_name -d -p $docker_port:$docker_web_port -it debian:latest && echo "Created a docker debian-based"
@@ -18,11 +20,11 @@ docker exec -ti $docker_name apt-get install ruby-dev zlib1g-dev liblzma-dev -y
 docker exec -ti $docker_name gem install rails && echo "Install rails"
 
 # Install some useful tools
-docker exec -ti $docker_name apt-get install vim curl -y
+docker exec -ti $docker_name apt-get install vim curl procps net-tools -y
 
 # Run a Rails project
-rail_name="my-blog"
 docker exec -ti $docker_name apt-get install libsqlite3-dev -y
-docker exec -ti $docker_name rails new ~/$rail_name && echo "Created new project"
+docker exec -ti $docker_name rails new $web_dir && echo "Created new project"
 docker exec -ti $docker_name apt-get install nodejs -y && echo "Installed Nodejs"
 docker exec -ti $docker_name rails s -b 0.0.0.0 && echo "Allowed all inbound connections"
+docker exec -d -it ruby bash -c "export web_dir=web_dir && cd $web_dir; rails s -b 0.0.0.0"
