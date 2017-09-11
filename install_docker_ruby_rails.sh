4459962729d92cb374ed9cbe0b9c81dc933d4e8a
Phuong Cao <caophuong2012@gmail.com>
2017-09-11 13:45:36 +0700

    Update install_docker_ruby_rails.sh
    - Add variable docker_web_port as 3000 (default of Rails)
    - Install vim and curl

--- install_docker_ruby_rails.sh
+++ install_docker_ruby_rails.sh
@@ -2,9 +2,10 @@
 echo "This script is tutored to install Ruby on docker"
 [ ! -z "$1" ] && docker_name=$1 || docker_name="ruby" 
 [ ! -z "$2" ] && docker_port=$2 || docker_port="3000"
+docker_web_port="3000"
 
 #docker create --name $docker_name -ti debian:latest
-docker run --name $docker_name -d -p $docker_port:80 -it debian:latest && echo "Created a docker debian-based"
+docker run --name $docker_name -d -p $docker_port:$docker_web_port -it debian:latest && echo "Created a docker debian-based"
 docker start $docker_name && echo "Started container"
 sleep 5
 
@@ -15,6 +16,9 @@ docker exec -ti $docker_name ruby -v && echo "Checked ruby version"
 docker exec -ti $docker_name apt-get install build-essential patch -y 
 docker exec -ti $docker_name apt-get install ruby-dev zlib1g-dev liblzma-dev -y
 docker exec -ti $docker_name gem install rails && echo "Install rails"
+
+# Install some useful tools
+docker exec -ti $docker_name apt-get install vim curl -y
 
 # Run a Rails project
 rail_name="my-blog"
